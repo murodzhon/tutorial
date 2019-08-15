b208bf143f5c657255be6b01484536e8e5ad65b9
murodzhon <murodzhon.akhmedov@irb.usi.ch>
2019-08-15 08:51:18 +0200

    Update

--- conf.py
+++ conf.py
@@ -71,7 +71,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'OmicsPlayground'
+project = 'Omics Playground'
 copyright = 'Murodzhon Akhmedov, 2019'
 
 # The version info for the project you're documenting, acts as replacement for
